acf015cc23167baf9fe7145e35a084572a8625aa
Alex Daughters <alexdaughters@gmail.com>
2023-02-02 21:32:00 +0000

    lab complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.fail;
 
 public class Lab {
     /**
@@ -20,6 +21,14 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
+        if(bool == true)
+        {
+            return x;
+        }
+        if(bool == false)
+        {
+            return y;
+        }
         return 0;
     }
 }
