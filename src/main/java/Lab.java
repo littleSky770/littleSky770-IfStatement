e7b0e933c2e18cfd28c9e1754043e58d34332b9b
Alex Daughters <alexdaughters@gmail.com>
2023-08-02 15:08:52 +0000

    completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -10,6 +10,7 @@ public class Lab {
      * if bool is true, return x.
      * if bool is false, return y.
      * 
+     * 
      * All variables needed to solve the problem are provided by the method parameters - boolean bool, int x, int y.
      * This means that you can use the variables bool, x, and y freely, within the if statement, to solve the problem.
      *
